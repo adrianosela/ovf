2c0046144048c440355f8a8756614cd43aa2eeb5
Adriano <adriano.selaviles@gmail.com>
2019-10-15 12:54:00 -0700

    latest

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+// main has a dangerous buffer overflow
 int main(int argc, char *argv[]) {
 	int authenticated = 0;
-        char buffer[5];
+
+	char buffer[5];
         strcpy(buffer, argv[1]);
 
 	if (authenticated) {
