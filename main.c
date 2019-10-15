c958785e97527555592643918c32b49f9e3adc20
Adriano <adriano.selaviles@gmail.com>
2019-10-15 13:26:28 -0700

    latest

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 int main(int argc, char *argv[]) {
 	int authenticated = 0;
 
-	char buffer[5];
-        strcpy(buffer, argv[1]);
+	char buffer[4];
+
+        // copy argument string to buffer
+	// with unsafe string copy func
+	strcpy(buffer, argv[1]);
 
 	if (authenticated) {
 		printf("user is authenticated!");
